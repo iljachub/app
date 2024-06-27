4912065713576587882c1439ea8494bc7b6195a0
illia <iljachub@gmail.com>
2024-06-27 04:59:51 +0300

    add Images

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 
-from re import DEBUG
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
-from app.settings import DEBUG
+from app import settings
+from app import settings
 
 
 urlpatterns = [
@@ -13,8 +14,10 @@ urlpatterns = [
 
 ]
 
-if DEBUG:
+if settings. DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     
